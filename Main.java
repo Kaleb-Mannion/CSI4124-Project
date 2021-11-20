67e20d5848d570418187f9a6688dac677399d95c
Kaleb-Mannion <kaleb.mannion@gmail.com>
2021-11-20 02:20:17 -0500

    Generated queue for testing, outputting test sequence
    Bugs: 
    - The cook never finishes cooking, so the program never halts, may be related to next bug
    - Interrupts don't look to be happening?
    - Cook can put more fries in fryer than has friers -> Solution: The issue really is that the task that "puts" the fries in the fryer doesn't use a fryer resource, so it will do it as much as it can, could probably fix it by implementing tasks that use multiple resources, or just rephrasing the descriptions (probably easier)

--- Main.java
+++ Main.java
@@ -1,67 +1,80 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class main {
 
-    static Queue<Task> taskQueue = new LinkedList<>();
-    static ArrayList<Cook> cookList = new ArrayList<>();
+    //static Queue serviceQueue = new LinkedList<>();
+    //static ArrayList<Cook> cookList = new ArrayList<>();
+
 
     public static void main(String[] args) {
-
-        cookList.add(new Cook(true, true, false));
-        cookList.add(new Cook(false, true, true));
-        cookList.add(new Cook(true, false, true));
-
-        taskQueue.addAll(createFries(0));
-        taskQueue.addAll(createNacho(1));
-        taskQueue.addAll(createFries(2));
-        taskQueue.addAll(createNacho(3));
-        taskQueue.addAll(createFries(4));
-        taskQueue.addAll(createNacho(5));
-
-        System.out.println(taskQueue.size());
-
-        while(!taskQueue.isEmpty()){
-            break;
+		LinkedList<task> taskList = new LinkedList<task>();//keep a list of all the current tasks
+		LinkedList<task> futureTaskList = new LinkedList<task>();//keep a list off all the future tasks
+		Cook cook = new Cook(4, 2, 4);//create a cook with 4 cook tops, 2 oven, 4 fryers
+		double currentTime = 0;//start time at 0
+        for (int i = 0; i < 10; i++){//create ten fry orders 60s apart to test the queuing system
+			futureTaskList.push(createFries(i, i*60));
         }
-
+		while(!taskList.isEmpty() || !futureTaskList.isEmpty()){//as long as there are future tasks or current tasks, keep working
+			boolean eventsSkipped = false;//keep track if any Tasks were skipped by a non interruptable task advancing time (see cook)
+			ListIterator<task> iteratorFutureTasks = futureTaskList.listIterator(0);//create an iterator for the future tasks
+			LinkedList<task> missedTasks = new LinkedList<>();//create a list of missed tasks
+			while(iteratorFutureTasks.hasNext()){//check if any tasks have been skipped over by an uninterruptable activity
+				task futureEvent = iteratorFutureTasks.next();
+				if(futureEvent.arrivalTime < currentTime){//if the current task occured in the past
+					eventsSkipped = true;//we know we skipped a task arriving
+					missedTasks.add(futureEvent);//add it to the missed task list
+					taskList.add(futureEvent);//add it to the active task list
+				}
+			}
+			ListIterator<task> missedIterator = missedTasks.listIterator();//iterator for the missed tasks
+			while(missedIterator.hasNext()){//iterate through the tasks that were missed
+				task missed = missedIterator.next();
+				futureTaskList.remove(missed);//remove the missed tasks from the future task list
+			}
+			if(!eventsSkipped){//if no events were skipped, we can advance time to the next task
+				iteratorFutureTasks = futureTaskList.listIterator(0);//create an iterator for the future tasks
+				task soonestEvent = null;//keep track of which task in the future arrives the soonest
+				while(iteratorFutureTasks.hasNext()){//iterate through the future tasks
+					task futureEvent = iteratorFutureTasks.next();
+					if(soonestEvent == null || soonestEvent.arrivalTime > futureEvent.arrivalTime){//if the soonest event hasnt been chosen yet, or the current event is sooner than the current soonest
+						soonestEvent = futureEvent;//replace the soonest task with the new task
+					}
+				}
+				if(soonestEvent != null){//if there was a next event
+					futureTaskList.remove(soonestEvent);//remove it from the future task list
+					taskList.add(soonestEvent);//add it to the current task list
+					currentTime = soonestEvent.arrivalTime;//jump time forward to the arrival of the task
+//					System.out.println("Order #" + Integer.toString(soonestEvent.orderID) + " received");
+				}
+			}
+			currentTime = cook.nextEvent(taskList, futureTaskList, currentTime);//have the cook check if the advancement of time has allowed them to work on a new task
+			//if(futureTaskList.isEmpty()){
+			//	return;
+			//}
+		}
         // Runs until list is depleted
-        while (!taskQueue.isEmpty()){
+        /*while (!serviceQueue.isEmpty()){
 
             // removes head of queue (represents customer being "serviced")
             //Customer current = serviceQueue.poll();
 
             // checks to see if the recent customer removal produces an empty queue
-            if (!taskQueue.isEmpty()) {
+            if (!serviceQueue.isEmpty()) {
 
                 //informs the head of the queue of the customer currently being serviced to calculate wait time.
                 //serviceQueue.peek().setFrontCustomer(current);
                 //serviceQueue.peek().calculateWaitTime();
 
             }
-
-            break;
-        }
+        }*/
     }
 
-    static LinkedList<Task> createFries(int orderID) {
-        LinkedList<Task> fries = new LinkedList<>();
-        fries.push(new Task("Cooktop", false, true, orderID, 10, 1, "Plating fries"));
-        fries.push(new Task("Cooktop", true, false, orderID, 3, 1, "Removing fries from deep fryer"));
-        fries.push(new Task("Deep Fryer", false, false, orderID, 120, 1, "Fries deep frying"));
-        fries.push(new Task("Cooktop", false, false, orderID, 10, 1, "Adding fries to deep fryer"));
-        return fries;
-    }
-
-    static LinkedList<Task> createNacho(int orderID) {
-        LinkedList<Task> nachos = new LinkedList<>();
-        nachos.push(new Task("Cooktop", false, true, orderID, 10, 1, "Plating nachos"));
-        nachos.push(new Task("Cooktop", true, false, orderID, 5, 1, "Removing nachos from oven"));
-        nachos.push(new Task("Oven", false, false, orderID, 50, 1, "Natchos cooking"));
-        nachos.push(new Task("Cooktop", false, false, orderID, 5, 1, "Adding nachos to oven"));
-        nachos.push(new Task("Cooktop", false, true, orderID, 120, 1, "Preparing nachos"));
-        return nachos;
-    }
-
+	static task createFries(int orderID, double arrivalTime){
+		task plating = new task("Cook", false, true, true, orderID, 10, 1, null,"Plating fries");
+		task removing = new task("Cook", true, false, false, orderID, 3, 1, plating,"Removing fries from deep fryer");
+		task frying = new task("Fryer", false, false, true, orderID,  120, 1, plating,"Fries deep frying");
+		task fries = new task("Cook", false, false, false, orderID, 10, 1, arrivalTime, frying,"Adding fries to deep fryer");
+		return fries;
+	}
 }
